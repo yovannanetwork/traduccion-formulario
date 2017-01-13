25bd0dcb0790c12c4770a0425e0b10b298861166
yovannanetwork <yovannanetwork@gmail.com>
2017-01-13 14:02:35 -0600

    js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML = "Por favor inicia secion";
+document.getElementById("inputEmail").placeholder = "Correo Electronico";
+document.getElementById("inputPassword").placeholder = "Contraseña";
+ document.querySelector("span").innerHTML = "Recordar datos";
+document.querySelector("button").innerHTML = "Iniciar Seciòn";
